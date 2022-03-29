8cc7a84de817b8286e038a7e7d05b8f9d154d955
Claribel <Claribel.D.Mendez@gmail.com>
2022-03-29 02:06:08 -0400

    add nav links

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,13 @@ function App() {
     <div className="App">
       Hello from Techtonica
               <div className="content">
+              <nav>
+        <ul>
+          <li><a href="/contacts">Contact Book</a></li>
+          <li><a href="/Form">Form</a></li>
+          {/* <li><a href="/whale">About Us</a></li> */}
+        </ul>
+      </nav>
 
       <Routes>
         <Route path="/form" element={<Form/>} />
